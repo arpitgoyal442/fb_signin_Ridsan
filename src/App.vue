dc78d559889c765a025f39cfadcf5dbffd3764c0
SIDHARTH CHOUDHARY <choudharysidharth082000@gmail.com>
2021-10-06 19:57:45 +0530

    added google Login

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     
     <signin/>
+    <GoogleLogin :params="params" :renderParams="renderParams" :onSuccess="onSuccess" :onFailure="onFailure"></GoogleLogin>j
   </div>
   
 </template>
@@ -9,12 +10,29 @@
 <script>
 
 import Signin from "./components/Signin.vue"
+import GoogleLogin from 'vue-google-login';
 
 export default {
   name: 'App',
   components: {
-    Signin
+    Signin,
+    GoogleLogin
     
+  },
+  data()
+  {
+      return {
+                // client_id is the only required property but you can add several more params, full list down bellow on the Auth api section
+                params: {
+                    client_id: "593324643569-8ev78dfkv9jocqts1h24n5sntgr8vmhc.apps.googleusercontent.com"
+                },
+                // only needed if you want to render the button with the google ui
+                renderParams: {
+                    width: 250,
+                    height: 50,
+                    longtitle: true
+                }
+            }
   }
 }
 </script>
